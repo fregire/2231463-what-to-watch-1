59c847efd136d4a426f93d080dfec606a78b22f8
Daniil Gilmutdinov <fregire@mail.ru>
2022-10-11 16:38:14 +0500

    Добавил маршруты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
 import MainScreen from '../../pages/main-screen/main-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
+import MovieScreen from '../../pages/movie-screen/movie-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   title: string;
@@ -7,7 +16,26 @@ type AppProps = {
 }
 
 function App({title, genre, year}: AppProps): JSX.Element {
-  return <MainScreen title={title} genre={genre} year={year} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainScreen title={title} genre={genre} year={year} />} />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <MyListScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.SignIn} element={<SignInScreen />} />
+        <Route path={AppRoute.Film} element={<MovieScreen />} />
+        <Route path={AppRoute.AddReview} element={<AddReviewScreen />} />
+        <Route path={AppRoute.Player} element={<PlayerScreen />} />
+        <Route path={AppRoute.Default} element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
