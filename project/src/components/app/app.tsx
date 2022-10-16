e079d91743c757804df4a59742834cae565e9452
Daniil Gilmutdinov <fregire@mail.ru>
2022-10-16 16:10:49 +0500

    Исправил замечания

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,15 @@
 import MainScreen from '../../pages/main-screen/main-screen';
+import { FC } from 'react';
 
-type AppProps = {
+type Props = {
   title: string;
   genre: string;
   year: number;
 }
 
-function App({title, genre, year}: AppProps): JSX.Element {
+const App: FC<Props> = (props) => {
+  const { title, genre, year } = props;
   return <MainScreen title={title} genre={genre} year={year} />;
-}
+};
 
 export default App;
--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,24 +1,24 @@
-function FilmCard(): JSX.Element {
-  return (
-    <article className="small-film-card catalog__films-card">
-      <div className="small-film-card__image">
-        <img
-          src="img/mindhunter.jpg"
-          alt="Mindhunter"
-          width="280"
-          height="175"
-        />
-      </div>
-      <h3 className="small-film-card__title">
-        <a
-          className="small-film-card__link"
-          href="film-page.html"
-        >
-          Mindhunter
-        </a>
-      </h3>
-    </article>
-  );
-}
+import { FC } from 'react';
+
+const FilmCard: FC = () => (
+  <article className="small-film-card catalog__films-card">
+    <div className="small-film-card__image">
+      <img
+        src="img/mindhunter.jpg"
+        alt="Mindhunter"
+        width="280"
+        height="175"
+      />
+    </div>
+    <h3 className="small-film-card__title">
+      <a
+        className="small-film-card__link"
+        href="film-page.html"
+      >
+        Mindhunter
+      </a>
+    </h3>
+  </article>
+);
 
 export default FilmCard;
