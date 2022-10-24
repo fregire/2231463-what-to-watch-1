15c47f08962dc03fea5db5d93a32c7876e64d28d
Daniil Gilmutdinov <fregire@mail.ru>
2022-10-24 18:22:16 +0500

    5.17. Больше подробностей (часть 1)

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,8 +1,10 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Film } from '../../types/film';
 import { getFilmUrl } from '../../utils';
+import VideoPlayer from '../video-player/video-player';
 
+const TIME_BEFORE_PLAYING_IN_MS = 1000;
 type Props = {
   film: Film;
   onMouseOver: (film: Film) => void;
@@ -10,20 +12,41 @@ type Props = {
 
 const FilmCard: FC<Props> = (props) => {
   const { film, onMouseOver } = props;
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [isNeedVideoToPlay, setIsNeedVideoToPlay] = useState(false);
+
+  useEffect(() => {
+    let needUpdate = true;
+
+    if (isNeedVideoToPlay) {
+      setTimeout(() => needUpdate && setIsVideoPlaying(true), TIME_BEFORE_PLAYING_IN_MS);
+    }
+
+    return () => {needUpdate = false;};
+  }, [isNeedVideoToPlay]);
+
+  const handleFilmCardMouseLeave = () => {
+    setIsNeedVideoToPlay(false);
+    setIsVideoPlaying(false);
+  };
 
   return (
     <article
       className="small-film-card catalog__films-card"
       onMouseOver={(evt) => {
         onMouseOver(film);
+        setIsNeedVideoToPlay(true);
       }}
+      onMouseLeave={handleFilmCardMouseLeave}
     >
       <div className="small-film-card__image">
-        <img
-          src={film.previewImage}
-          alt={film.name}
-          width="280"
-          height="175"
+        <VideoPlayer
+          width={280}
+          height={175}
+          poster={film.previewImage}
+          muted
+          src={film.videoLink}
+          isPlaying={isVideoPlaying}
         />
       </div>
       <h3 className="small-film-card__title">
---
+++ project/src/components/video-player/video-player.tsx
@@ -0,0 +1,32 @@
+import { FC, useRef, useEffect } from 'react';
+
+type Props = {
+  src: string;
+  poster: string;
+  muted: boolean;
+  width: number;
+  height: number;
+  isPlaying: boolean;
+}
+
+const VideoPlayer: FC<Props> = (props) => {
+  const { src, poster, muted, width, height, isPlaying } = props;
+  const videoPlayerRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (videoPlayerRef === null) {
+      return;
+    }
+
+    if (isPlaying) {
+      videoPlayerRef.current?.play();
+    } else {
+      videoPlayerRef.current?.load();
+    }
+
+  }, [isPlaying]);
+
+  return <video ref={videoPlayerRef} width={width} height={height} src={src} poster={poster} muted={muted}></video>;
+};
+
+export default VideoPlayer;
